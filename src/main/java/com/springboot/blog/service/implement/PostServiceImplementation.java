96c4f6b0237481d571204c19b9778f397ec9083f
milton munoz <livemilton@hotmail.com>
2025-05-25 21:33:45 -0500

    blog service - Delete post REST API -coding

--- src/main/java/com/springboot/blog/service/implement/PostServiceImplementation.java
+++ src/main/java/com/springboot/blog/service/implement/PostServiceImplementation.java
@@ -59,6 +59,15 @@ public class PostServiceImplementation implements PostService {
         return mapToDTO(updatePost);
     }
 
+    @Override
+    public void deletePostById(long id) {
+        //get post by id from the database
+        Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post", "id", id));
+
+        postRepository.delete(post);
+
+    }
+
     // convert Entity into DTO
     private PostDto mapToDTO(Post post){
         PostDto postDto = new PostDto();
