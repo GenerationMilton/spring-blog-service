8f0f526989cb3f64e0a352ea4a5adf66eafbf5a1
milton munoz <livemilton@hotmail.com>
2025-05-25 21:21:41 -0500

    blog service - Update post REST API -coding

--- src/main/java/com/springboot/blog/service/implement/PostServiceImplementation.java
+++ src/main/java/com/springboot/blog/service/implement/PostServiceImplementation.java
@@ -46,6 +46,19 @@ public class PostServiceImplementation implements PostService {
         return mapToDTO(post);
     }
 
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        //get post by id from the database
+        Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post", "id", id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatePost = postRepository.save(post);
+        return mapToDTO(updatePost);
+    }
+
     // convert Entity into DTO
     private PostDto mapToDTO(Post post){
         PostDto postDto = new PostDto();
