02a4a2cc149d481e9611dadc4728ac9a15cde2d2
Milton Munoz <livemilton@gmail.com>
2025-06-02 20:49:35 -0500

    blog service - Creating JPA Entity - comment

--- src/main/java/com/springboot/blog/entitiy/Post.java
+++ src/main/java/com/springboot/blog/entitiy/Post.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Data
 @AllArgsConstructor
@@ -20,10 +23,16 @@ public class Post {
             strategy = GenerationType.IDENTITY
     )
     private Long id;
+
     @Column(name = "title", nullable = false)
     private String title;
+
     @Column(name = "description", nullable = false)
     private String description;
+
     @Column(name = "content", nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 }
