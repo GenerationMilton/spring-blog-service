39333913091f99da8d3d01fc06480cb87d7d23ff
Milton Munoz <livemilton@gmail.com>
2025-06-02 20:28:37 -0500

    blog service - Refactoring Hardcoded values for paging and sorting

---
+++ src/main/java/com/springboot/blog/utils/AppConstants.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.utils;
+
+public class AppConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+    public static final String DEFAULT_PAGE_NUMBER_NAME = "pageNo";
+    public static final String DEFAULT_PAGE_SIZE_NAME = "pageSize";
+    public static final String DEFAULT_SORT_BY_NAME ="sortBy";
+    public static final String DEFAULT_SORT_DIRECTION_NAME ="sortDir";
+}
