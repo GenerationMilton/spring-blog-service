daef68feb26cb3aa6bd135a313e6ca26839f36d1
Milton Munoz <livemilton@gmail.com>
2025-06-04 22:33:41 -0500

    blog service - Update Comment By id REST API

--- src/main/java/com/springboot/blog/service/implement/CommentServiceImplementation.java
+++ src/main/java/com/springboot/blog/service/implement/CommentServiceImplementation.java
@@ -70,6 +70,28 @@ public class CommentServiceImplementation implements CommentService {
         return mapToDTO(comment);
     }
 
+    @Override
+    public CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest) {
+
+        // retrieve post entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        // retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow( ()-> new ResourceNotFoundException("Comment","id", commentId));
+
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belongs to post");
+        }
+
+        comment.setName(commentRequest.getName());
+        comment.setEmail(commentRequest.getEmail());
+        comment.setBody(commentRequest.getBody());
+
+        Comment updateComment = commentRepository.save(comment);
+
+        return mapToDTO(updateComment);
+    }
+
     private CommentDto mapToDTO(Comment comment){
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
