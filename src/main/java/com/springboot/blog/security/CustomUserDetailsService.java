961da58f0c8bdb84bf068d5297752b69332aa8da
Milton Munoz <livemilton@gmail.com>
2025-06-11 18:56:55 -0500

    blog service - UserDetailsService interface implementation CustomUserDetailsService

---
+++ src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.springboot.blog.security;
+
+import com.springboot.blog.entitiy.User;
+import com.springboot.blog.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found with username or email"+ usernameOrEmail));
+        //logic to convert a list of set of Role into set of GrantedAuthority
+        Set<GrantedAuthority> authorities = user
+                    .getRoles()
+                    .stream()
+                    .map((role)-> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        //logic to convert User object into a spring security provided User object
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+}
