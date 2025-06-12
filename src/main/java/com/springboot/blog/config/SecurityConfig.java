abd9ab65410713e515fd5cf10d175f16cc9516e1
Milton Munoz <livemilton@gmail.com>
2025-06-11 19:52:01 -0500

    blog service - Securing REST API'S with Database Authentication

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.springboot.blog.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +21,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+
     //Encode the password
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -36,6 +52,7 @@ public class SecurityConfig {
     }
 
     //Users admin and user
+    /*
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails userSecure = User.builder()
@@ -52,4 +69,6 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(userSecure, admin);
     }
+    */
+
 }
--- src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
+++ src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
@@ -14,13 +14,17 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
 
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
 
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElseThrow(()-> new UsernameNotFoundException("User not found with username or email"+ usernameOrEmail));
