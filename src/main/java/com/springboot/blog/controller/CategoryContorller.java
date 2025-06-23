dca145d3cc7f0c8c8b32813943d0a6b14d828833
Milton Munoz <livemilton@gmail.com>
2025-06-23 09:31:53 -0500

    blog service - :sparkles: build addCategory RESTAPI and secure using ADMIN role

---
+++ src/main/java/com/springboot/blog/controller/CategoryContorller.java
@@ -0,0 +1,31 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryContorller {
+
+    private CategoryService categoryService;
+
+    public CategoryContorller(CategoryService categoryService) {
+        this.categoryService = categoryService;
+
+    }
+
+    //Build add Category REST API
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto){
+        CategoryDto savedCategory = categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+}
